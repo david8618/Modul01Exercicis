c72f3318b21f0a54f7c68f5a1d0d3b0480eaaec6
david8618 <ddiaz.8618@gmail.com>
2021-07-19 07:07:33 +0200

    Exercici Nivelltres corregit.

--- src/david/itacademy/M01Nivell03/NivellTres.java
+++ src/david/itacademy/M01Nivell03/NivellTres.java
@@ -11,21 +11,23 @@ public class NivellTres {
 	 */
         //Array int amb valors del 1 al 10
         int[] miArray = {1,2,3,4,5,6,7,8,9,10};
-        //Recorro el array inicialitzat
+        //Variable Auxiliar
+        int numActual;
+        //Imprimeixo l'array en l'ordre correcte
+        for (int i:miArray) { System.out.print(i + " "); }
+        //Utilitzo el mètode bombolla invertit
         for (int i = 0; i < miArray.length; i++) {
-            //ensenyo por pantalla els valor del array
-            System.out.print(miArray[i]+ " ");
-            /*
-            L'indico amb un condicional que quan el valor de i sigui 10,
-            recorri l'array en ordre invertit.
-             */
-            if(miArray[i]==10){
-                System.out.println();
-                for (int k = miArray.length-1; k >=0; k--) {
-                    System.out.print(miArray[k]+" ");
-                    //Quan arriba al valor 1 surt del loop
+            for (int j=0; j < miArray.length-1; j++) {
+                if(miArray[j]<miArray[j+1]) { //Si número actual es < número següent
+                    //Utilitzo la variable Auxiliar
+                    numActual = miArray[j]; //la nova variable guardara el valor del numero actual del array
+                    miArray[j] = miArray[j + 1]; //al array li donem el valor del numero següent
+                    miArray[j + 1] = numActual; // finalment el número següent li donem el valor que hi havia a la variable auxiliar
                 }
             }
         }
+        System.out.println();
+        //Imprimim per pantalla la array ordenat al reves
+        for (int j: miArray) { System.out.print(j + " "); }
     }
 }
